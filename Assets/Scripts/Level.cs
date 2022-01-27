0e0815d6433d09fbeade71e2bd8c57f5e5cc87b3
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-01-28 00:26:29 +0300

    level script

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] Tube[] _tubes;
+
+    public bool CheckWin()
+    {
+
+        foreach (var item in _tubes)
+        {
+            if (!item.IsRightOrder()) return false;
+        }
+
+        return true;
+    }
+}
